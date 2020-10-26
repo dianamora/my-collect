39cabd25d95bf6d8235be08e495848aa2051cf7e
Diana Mora <morra.dianna@gmail.com>
2020-10-26 16:44:53 -0400

    commit tree 08b2e28952f9b78db734b33b4889a36067f666e1

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+# def hello(array)
+#     i = 0
+#     collection = []
+#     while i < array.length
+#       collection << yield(array[i])
+#       i += 1
+#     end
+#     collection
+#   end 
+  
+  
+#   hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" } 
 
+def my_collect(array)
+    i = 0 
+    new_array = []
+    while i < array.length
+        new_array << yield(array[i])
+        i += 1
+    end
+    new_array
+end   
 
